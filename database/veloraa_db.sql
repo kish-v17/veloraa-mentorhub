5cf20890012ae501f0bd8001f798b230bd57ad12
Jinal Taraviya <155702779+JinalTaraviya22@users.noreply.github.com>
2024-09-19 14:58:18 +0530

    db

---
+++ database/veloraa_db.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 19, 2024 at 11:27 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `veloraa_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follow_tbl`
+--
+
+CREATE TABLE `follow_tbl` (
+  `F_MentorId` int(11) DEFAULT NULL,
+  `F_FollowedById` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_tbl`
+--
+
+CREATE TABLE `user_tbl` (
+  `U_Id` int(11) NOT NULL,
+  `U_Fnm` varchar(60) NOT NULL,
+  `U_Email` varchar(60) NOT NULL,
+  `U_Phn` varchar(60) NOT NULL,
+  `U_Pwd` varchar(255) NOT NULL,
+  `U_City` varchar(60) NOT NULL,
+  `U_State` varchar(60) NOT NULL,
+  `U_Country` varchar(60) NOT NULL,
+  `U_About` text NOT NULL,
+  `U_GitHub` varchar(60) NOT NULL,
+  `U_LinkedIn` varchar(60) NOT NULL,
+  `U_Skill` text NOT NULL,
+  `U_Profile` text NOT NULL,
+  `U_Status` tinyint(1) NOT NULL,
+  `U_Fee` decimal(10,0) NOT NULL,
+  `U_Role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_tbl`
+--
+
+INSERT INTO `user_tbl` (`U_Id`, `U_Fnm`, `U_Email`, `U_Phn`, `U_Pwd`, `U_City`, `U_State`, `U_Country`, `U_About`, `U_GitHub`, `U_LinkedIn`, `U_Skill`, `U_Profile`, `U_Status`, `U_Fee`, `U_Role`) VALUES
+(1, 'rixit', 'rixit05@gmail.com', '', '$2y$10$mT3lJGYokfyJQUrlBtg1DeFfsLbBF97tX0RgWjfUj1O72BxO3oBE6', '', '', '', '', '', '', '', '', 0, 0, 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `follow_tbl`
+--
+ALTER TABLE `follow_tbl`
+  ADD KEY `F_MentorId` (`F_MentorId`),
+  ADD KEY `F_FollowedById` (`F_FollowedById`);
+
+--
+-- Indexes for table `user_tbl`
+--
+ALTER TABLE `user_tbl`
+  ADD PRIMARY KEY (`U_Id`),
+  ADD UNIQUE KEY `unique_email` (`U_Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_tbl`
+--
+ALTER TABLE `user_tbl`
+  MODIFY `U_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `follow_tbl`
+--
+ALTER TABLE `follow_tbl`
+  ADD CONSTRAINT `follow_tbl_ibfk_1` FOREIGN KEY (`F_MentorId`) REFERENCES `user_tbl` (`U_Id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `follow_tbl_ibfk_2` FOREIGN KEY (`F_FollowedById`) REFERENCES `user_tbl` (`U_Id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
