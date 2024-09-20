39de2d5433f215b66aba09112ec932da35853d36
Kish-v17 <kish.v07@gmail.com>
2024-09-20 10:20:43 +0530

    update

--- mentee/account-information.php
+++ mentee/account-information.php
@@ -185,14 +185,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                     </div>
                                 </div>
                             </div>
-
-
-
-
-
-
-
-
+                            
 
                             <!-- Skills Modal -->
                             <div class="modal fade" id="skillsModal" tabindex="-1" aria-labelledby="skillsModalLabel" aria-hidden="true">
@@ -200,9 +193,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="skillsModalLabel">Select Skills</h5>
-                                            <button type="button" data-bs-target="#Modalstory" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
-                                        <!-- <div class="modal-body">
+                                        <div class="modal-body">
                                             <input type="text" id="skillsSearch" class="form-control mb-3" placeholder="Search for skills...">
                                             <div id="skillsList">
                                                 <label class="form-check">
@@ -220,29 +213,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                                 <label class="form-check">
                                                     <input type="checkbox" name="skill[]" value="php" <?php echo in_array('php', explode(',', $skills)) ? 'checked' : ''; ?>> PHP
                                                 </label>
-                                            </div>
-                                        </div> -->
-
-                                        <div class="modal bottom side fade" id="Modalstory" tabindex="-1" role="dialog" style=" overflow-y: auto;">
-                                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                                <div class="modal-content border-0 bg-transparent">
-                                                    <button type="button" class="close mt-0 position-absolute top--30 right--10" data-dismiss="modal" aria-label="Close"><i class="ti-close text-grey-900 font-xssss"></i></button>
-                                                    <div class="modal-body p-0">
-                                                        <div class="card w-100 border-0 rounded-3 overflow-hidden bg-gradiant-bottom bg-gradiant-top">
-                                                            <div class="owl-carousel owl-theme dot-style3 story-slider owl-dot-nav nav-none">
-                                                                <div class="item"><img src="images/story-5.jpg" alt="image"></div>
-                                                                <div class="item"><img src="images/story-6.jpg" alt="image"></div>
-                                                                <div class="item"><img src="images/story-7.jpg" alt="image"></div>
-                                                                <div class="item"><img src="images/story-8.jpg" alt="image"></div>
-
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group mt-3 mb-0 p-3 position-absolute bottom-0 z-index-1 w-100">
-                                                            <input type="text" class="style2-input w-100 bg-transparent border-light-md p-3 pe-5 font-xssss fw-500 text-white" value="Write Comments">
-                                                            <span class="feather-send text-white font-md text-white position-absolute" style="bottom: 35px;right:30px;"></span>
-                                                        </div>
-                                                    </div>
-                                                </div>
                                             </div>
                                         </div>
                                         <div class="modal-footer">
